1c95dbad5d6c02d40cef167a0fd643141ce49b52
Ayaka Kawabe <kawabe.ayaka@icloud.com>
2023-10-10 15:18:50 +0900

    アバターを追加

--- frontend/front/src/Unity/Unity.js
+++ frontend/front/src/Unity/Unity.js
@@ -1,1 +1,16 @@
-aaa
+import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
+import './Unity.css';
+
+function App() {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "build/build_connect_to_backend.loader.js",
+    dataUrl: "build/build_connect_to_backend.data",
+    frameworkUrl: "build/build_connect_to_backend.framework.js",
+    codeUrl: "build/build_connect_to_backend.wasm",
+  });
+
+  return <Unity unityProvider={unityProvider}  className="Unity"/>;
+}
+
+export default App;
--- frontend/front/src/index.js
+++ frontend/front/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Unity_build from './Unity/Unity';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Unity_build />
   </React.StrictMode>
 );

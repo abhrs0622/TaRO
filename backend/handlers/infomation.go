6f92046511d51a4b8287bb5cadd2c7fd1c0b7cba
RM <ysnbtkgw@gmail.com>
2023-10-12 22:56:36 +0900

    infomation.go created

---
+++ backend/handlers/infomation.go
@@ -0,0 +1,54 @@
+package handlers
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+// ロード画面でアバターが話す内容を返す関数
+//　json形式で地名を受け取り，一度chatGPTのAPIに「（地名）に関連した豆知識を教えて」という文章をPOSTし，返ってきた文章をjson形式で返す
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+func Infomation(c *gin.Context) {
+	// 地名を受け取る
+	place := c.PostForm("place")
+	
+	// chatGPTのAPIにPOSTする
+	type RequestBody struct {
+		Text string `json:"text"`
+	}
+	reqBody := RequestBody{Text: place + "に関連した豆知識を教えて"}
+	reqBodyBytes, err := json.Marshal(reqBody)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "failed to marshal request body"})
+		return
+	}
+
+	client := openai.NewClient("YOUR_API_KEY")
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Hello!",
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+	// ここで返ってきた文章をjson形式で返す
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "handlers test"})
+}
+

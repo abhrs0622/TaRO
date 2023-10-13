c2b9fff93068fc030817fe4dad5ad7eb8bfc31a7
Your Name <zbnadrsplsgame@ezweb.ne.jp>
2023-10-13 12:22:42 +0900

    avatar

--- backend/handlers/avatar.go
+++ backend/handlers/avatar.go
@@ -7,5 +7,11 @@ import (
 )
 
 func Avatar(c *gin.Context) {
-	c.IndentedJSON(http.StatusCreated, gin.H{"message": "handlers test"})
+	// 送信されたリクエストデータからアバターの名前とアバターとの関係性をJSONにして返す
+	name := c.PostForm("name")
+	relationship := c.PostForm("relationship")
+	c.IndentedJSON(http.StatusCreated, gin.H{
+		"name":         name,
+		"relationship": relationship,
+	})
 }

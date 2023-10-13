1c44cf185ea20cf33e4a86395d93cde28ee5e58c
tomoish <tomo.i.120608@gmail.com>
2023-10-13 15:58:08 +0900

    Add openai in map.go

--- backend/handlers/map.go
+++ backend/handlers/map.go
@@ -1,11 +1,37 @@
 package handlers
 
 import (
+	"context"
 	"net/http"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/sashabaranov/go-openai"
 )
 
 func Map(c *gin.Context) {
-	c.IndentedJSON(http.StatusCreated, gin.H{"message": "handlers test"})
+	value := c.Query("value")
+
+	client := openai.NewClient("")
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Hello!",
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+
+	c.IndentedJSON(http.StatusCreated, gin.H{"message": value})
 }

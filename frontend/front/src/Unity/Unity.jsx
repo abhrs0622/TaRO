ff69ba3c1b8571a6b05478206cac62a99ea1034c
Ayaka Kawabe <kawabe.ayaka@icloud.com>
2023-10-14 14:26:51 +0900

    Avatar Actionsの切り出し

--- frontend/front/src/Unity/Unity.jsx
+++ frontend/front/src/Unity/Unity.jsx
@@ -1,9 +1,21 @@
 import React, { Fragment, useState, useCallback, useEffect } from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
+import * as AvatarActions from "./AvatarActions";
 
-function App() {
-  const [text, setText] = useState();
-  const [startTime, setStartTime] = useState();
+let actionStatus = { "setting": false, "start": false, "rootSearch": false, "finishPootSearch": false, "decideRoot": false, "move": false, "arrive": false };
+let moveSectionId = 1;
+let arrivePlace = "金閣寺";
+let userMessage = "メッセージ";
+AvatarActions.changeAllStatus(false);
+
+export function Avatar() {
+
+  // avatar data
+
+  const [text, setText] = useState("");
+  const [startTime, setStartTime] = useState("");
+
+  //Connect Unity
 
   const { unityProvider, sendMessage, addEventListener, removeEventListener } = useUnityContext({
     loaderUrl: "build/build_v2.5.loader.js",
@@ -11,6 +23,9 @@ function App() {
     frameworkUrl: "build/build_v2.5.framework.js",
     codeUrl: "build/build_v2.5.wasm",
   });
+
+
+  //take data from unity to react
 
   const handleText = useCallback((text) => {
     setText(text);
@@ -33,9 +48,13 @@ function App() {
     };
   }, [addEventListener, removeEventListener, handleStartTime]);
 
+
+  //send data from react to unity
+
   const googleApiKey = process.env.REACT_APP_GOOGLE_RECOGNITION_API_KEY;
   const backendApiServerUrl = process.env.REACT_APP_BACKEND_API_SERVER_URL;
   const VoicevoxEndpointUrl = process.env.REACT_APP_VOICEVOX_ENDPOINT_URL;
+
   function configUnityApi() {
     sendMessage("ChatdollKitVRM", "googleApiKey", googleApiKey);
     sendMessage("ChatdollKitVRM", "WakewordGoogleApiKey", googleApiKey);
@@ -47,49 +66,87 @@ function App() {
   configUnityApi();
 
 
-  function awake() {
-    sendMessage("ChatdollKitVRM", "awake");
-  }
-  function sleep() {
-    sendMessage("ChatdollKitVRM", "sleep");
-  }
-  function setting() {
-    sendMessage("ChatdollKitVRM", "setting");
-  }
-  function start() {
-    sendMessage("ChatdollKitVRM", "start");
-  }
-  function rootSearch() {
-    sendMessage("ChatdollKitVRM", "rootSearch");
-  }
-  function finishRootSearch() {
-    sendMessage("ChatdollKitVRM", "finishRootSearch");
-  }
-  function decideRoot() {
-    sendMessage("ChatdollKitVRM", "decideRoot");
-  }
-  const moveSectionId = 1;
-  function move() {
-    sendMessage("ChatdollKitVRM", "move", moveSectionId);
-  }
-  const arrivePlace = "金閣寺";
-  function arrive() {
-    sendMessage("ChatdollKitVRM", "arrive", arrivePlace);
-  }
-  const userMessage = "メッセージ";
-  function sendMessageToAvatar() {
-    sendMessage("ChatdollKitVRM", "sendMessageToAvatar", userMessage);
-  }
+  const changeAllStatus = (isAllStatus) => {
+    actionStatus.setting = isAllStatus;
+    actionStatus.start = isAllStatus;
+    actionStatus.rootSearch = isAllStatus;
+    actionStatus.finishPootSearch = isAllStatus;
+    actionStatus.decideRoot = isAllStatus;
+    actionStatus.move = isAllStatus;
+    actionStatus.arrive = isAllStatus;
+  };
+
+  //avatar actions
+  /*
+    function awake() {
+      sendMessage("ChatdollKitVRM", "awake");
+    }
+    function sleep() {
+      sendMessage("ChatdollKitVRM", "sleep");
+    }
+    function setting() {
+      changeAllStatus(false);
+      actionStatus.setting = true;
+      console.log(actionStatus.setting);
+      sendMessage("ChatdollKitVRM", "setting");
+    }
+    function start() {
+      changeAllStatus(false);
+      actionStatus.start = true;
+      sendMessage("ChatdollKitVRM", "start");
+    }
+    function rootSearch() {
+      changeAllStatus(false);
+      actionStatus.rootSearch = true;
+      sendMessage("ChatdollKitVRM", "rootSearch");
+    }
+    function finishRootSearch() {
+      changeAllStatus(false);
+      actionStatus.finishPootSearch = true;
+      sendMessage("ChatdollKitVRM", "finishRootSearch");
+    }
+    function decideRoot() {
+      changeAllStatus(false);
+      actionStatus.decideRoot = true;
+      sendMessage("ChatdollKitVRM", "decideRoot");
+    }
+    function move() {
+      changeAllStatus(false);
+      actionStatus.move = true;
+      sendMessage("ChatdollKitVRM", "move", moveSectionId);
+    }
+    function arrive() {
+      changeAllStatus(false);
+      actionStatus.arrive = true;
+      sendMessage("ChatdollKitVRM", "arrive", arrivePlace);
+    }
+    function sendMessageToAvatar() {
+      sendMessage("ChatdollKitVRM", "sendMessageToAvatar", userMessage);
+    }
+  */
+
+  const textLength = text.length;
+  const waitTimeFromStartVoice = 0.2 * Number(textLength) * 1000;
+
+  const [buttonVisibility, setButtonVisibility] = useState('hidden');
+
+  const displayNextButton = () => {
+    setTimeout(function () {
+      console.log("ボタン表示処理");
+      if (actionStatus.setting == true) {
+        setButtonVisibility('visible');
+      }
+    }, waitTimeFromStartVoice);
+  };
+  useEffect(displayNextButton, [startTime]);
   return (
     <Fragment>
-      <Unity unityProvider={unityProvider} />
-      <button onClick={setting}>Setting</button>
-      <button onClick={awake}>wake</button>
-      <button onClick={sleep}>sleep</button>
-
-      <p>{`${text},${startTime}`}</p>
+      <div class="Avatar">
+        <div><p>{`${text}`}</p></div>
+        <Unity unityProvider={unityProvider} />
+        <button onClick={() => AvatarActions.setting(sendMessage)}>Setting</button>
+        <button style={{ visibility: buttonVisibility }}>next</button>
+      </div>
     </Fragment>
   );
 }
-
-export default App;

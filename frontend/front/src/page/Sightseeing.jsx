7e3bd1a9c2072a8643466adac7af0844bf3dcc74
T.Tsutsui <huzyu2121@gmail.com>
2023-10-15 10:30:35 +0900

    sightseeingまだ修正中

--- frontend/front/src/page/Sightseeing.jsx
+++ frontend/front/src/page/Sightseeing.jsx
@@ -1,12 +1,6 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { useState } from 'react';
 import Street from "../components/StreetView";
 import Button from "../components/Button";
-
-const DataContext = createContext();
-
-export const useData = () => {
-  return useContext(DataContext);
-};
 
 const Sightseeing = () => {
   const position = [[33.56793280754822, 133.6677199608504], [34.344327666436314, 134.0426996864438], [34.265074709977505, 134.24368309932896]]; // 位置情報の配列
@@ -17,15 +11,19 @@ const Sightseeing = () => {
       setI(i + 1);
     }
   };
-  console.log({i})
-  if (i === 2){
+  if (i === (position.length -1)){
     return(
       <div>
         <Street
           initialLatitude={position[i][0]}
           initialLongitude={position[i][1]}
         />
-        <Button to="/" label="owari" hiddenButtonId="startHiddenButton" />
+        {/* <button onClick={incrementI}>owata</button> */}
+        <Button
+          label="次"
+          hiddenButtonId="hiddenButton"
+          onClick={incrementI}
+        />
     </div>
     );
   }else{
@@ -35,7 +33,7 @@ const Sightseeing = () => {
         initialLatitude={position[i][0]}
         initialLongitude={position[i][1]}
       />
-    <Button to="/Load" label="load" hiddenButtonId="startHiddenButton" onClick={incrementI}/>
+    <button onClick={incrementI}>次</button>
       </div>
     )
   }
@@ -43,3 +41,49 @@ const Sightseeing = () => {
 }
 
 export default Sightseeing;
+
+// import React, { createContext, useContext, useState } from 'react';
+// import Street from "../components/StreetView";
+// import Button from "../components/Button";
+
+// const DataContext = createContext();
+
+// export const useData = () => {
+//   return useContext(DataContext);
+// };
+
+// const Sightseeing = () => {
+//   const position = [[33.56793280754822, 133.6677199608504], [34.344327666436314, 134.0426996864438], [34.265074709977505, 134.24368309932896]]; // 位置情報の配列
+//   const [i, setI] = useState(0);
+
+//   const incrementI = () => {
+//     if (i < position.length - 1) {
+//       setI(i + 1);
+//     }
+//   };
+//   console.log({i})
+//   if (i === 2){
+//     return(
+//       <div>
+//         <Street
+//           initialLatitude={position[i][0]}
+//           initialLongitude={position[i][1]}
+//         />
+//         <Button to="/" label="owari" hiddenButtonId="startHiddenButton" />
+//     </div>
+//     );
+//   }else{
+//     return(
+//       <div>
+//       <Street
+//         initialLatitude={position[i][0]}
+//         initialLongitude={position[i][1]}
+//       />
+//     <Button to="/Load" label="load" hiddenButtonId="startHiddenButton" onClick={incrementI}/>
+//       </div>
+//     )
+//   }
+
+// }
+
+// export default Sightseeing;

cd6f41d68acbe6b08685a9672ca7f32a00ca1425
tomoish <tomo.i.120608@gmail.com>
2023-10-15 10:45:03 +0900

    Add name, relationship reducer

--- frontend/front/src/store.js
+++ frontend/front/src/store.js
@@ -4,6 +4,8 @@ import longitudeReducer from "./features/longitude/longitudeSlice";
 import plansReducer from "./features/plans/plansSlice";
 import selectedPlanReducer from "./features/selectedPlan/selectedPlanSlice";
 import coordinateReducer from "./features/coordinate/coordinateSlice";
+import nameReducer from "./features/name/nameSlice";
+import relationshipReducer from "./features/relationship/relationshipSlice";
 
 export default configureStore({
   reducer: {
@@ -12,5 +14,7 @@ export default configureStore({
     plans: plansReducer,
     selectedPlan: selectedPlanReducer,
     coordinate: coordinateReducer,
+    name: nameReducer,
+    relationship: relationshipReducer,
   },
 });

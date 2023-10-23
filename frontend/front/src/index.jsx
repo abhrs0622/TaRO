9334366966e80439f4cb58594e14e2de06a8c172
tomoish <tomo.i.120608@gmail.com>
2023-10-23 23:58:29 +0900

    Change ConfirmButton fetch

--- frontend/front/src/index.jsx
+++ frontend/front/src/index.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import store from "./store";
 import { Provider } from "react-redux";
+import App from "./App";
 import { Avatar } from "./Unity/Unity";
+import "./index.css";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

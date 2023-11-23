e08b17c3b6105dda9590ad09b2edba3f80a9eab2
ayakakawabe <ayaka.kawabe@icloud.com>
2023-11-23 18:58:04 +0900

    change load page css

--- frontend/front/src/page/Load.jsx
+++ frontend/front/src/page/Load.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import YouTube from '../components/YouTube';
 
 import Button from "../components/Button";
+import "./css/Load.css";
 
 const Load = () => {
   return (
     <div>
-      <YouTube />
+      <h2>移動中...</h2>
+      <div className='video'>
+        <YouTube />
+      </div>
       <div className="next_button_parent">
         <Button to="/Sightseeing" label="次の場所へ" hiddenButtonId="arrive1HiddenButton" />
       </div>

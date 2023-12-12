45dd3f85fc5787706882eda62917600f9483e484
RM <ysnbtkgw@gmail.com>
2023-12-12 20:25:52 +0900

    avatar.go edited

--- backend/producer/main.go
+++ backend/producer/main.go
@@ -10,8 +10,8 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 
-	"github.com/abhrs0622/TaRO/handlers"
-	"github.com/gin-contrib/cors"
+	// "github.com/abhrs0622/TaRO/handlers"
+	// "github.com/gin-contrib/cors"
 )
 
 var ginLambda *ginadapter.GinLambda
@@ -25,12 +25,12 @@ func init() {
     config.AllowOrigins = []string{"http://localhost:3000"} //許可するオリジンを記述：今回はフロントからのアクセスを許可したいので，フロントのURLを記述
     router.Use(cors.New(config))
 
-	router.POST("/communication", handlers.Communication)
+	//router.POST("/communication", handlers.Communication)
 	router.GET("/avatar", handlers.Avatar)
-	router.GET("/map", handlers.Map)
+	// router.GET("/map", handlers.Map)
 	router.GET("/get-plans", handlers.GetPlans)
 
-	// router.Run("localhost:8080")
+	router.Run("localhost:8080")
 
 	ginLambda = ginadapter.New(router)
 }

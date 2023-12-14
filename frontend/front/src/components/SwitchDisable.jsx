7a830fed2c04423d980366dc9115a7d05d112c16
ayakakawabe <ayaka.kawabe@icloud.com>
2023-12-14 19:40:01 +0900

    delete dead code in switch button disable

--- frontend/front/src/components/SwitchDisable.jsx
+++ frontend/front/src/components/SwitchDisable.jsx
@@ -5,17 +5,14 @@ export const SwitchDisable = () => {
     const [disable, setDisable] = useState(true);
     const [disableStyle, setDisableStyle] = useState({ opacity: 0.6 })
     useEffect(() => {
-        console.log("page settings")
     }, [])
     const startTime = ManageDisplyButton().startTime;
     const interval = Number(ManageDisplyButton().interval);
     useEffect(() => {
         if (startTime !== "") {
-            console.log("statTime change")
             setTimeout(() => {
                 setDisable(false)
                 setDisableStyle({ opacity: 1 })
-                console.log("disable false")
             }, interval)
         }
     }, [startTime])

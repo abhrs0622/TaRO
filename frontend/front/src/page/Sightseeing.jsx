c2111dcba2c9b89b17fa9fcd58555171d335f63b
T.Tsutsui <huzyu2121@gmail.com>
2023-12-15 01:56:39 +0900

    ストリートビューが自動で変わる

--- frontend/front/src/page/Sightseeing.jsx
+++ frontend/front/src/page/Sightseeing.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import Button from "../components/Button";
-import Map1 from "../components/Map1";
+import ReactStreetview from "react-google-streetview";
 
 const Sightseeing = () => {
   const coordinate = useSelector((state) => state.coordinate.value);
@@ -11,14 +11,47 @@ const Sightseeing = () => {
     [parseFloat(coordinate[2][0]), parseFloat(coordinate[2][1])],
   ]; // 位置情報の配列
   const [i, setI] = useState(0);
+  const [positions, setPositions] = useState({
+    lat: position[i][0],
+    lng: position[i][1],
+  });
   const incrementI = () => {
     if (i < position.length - 1) {
       setI(i + 1);
+      setPositions({
+        lat: position[i + 1][0],
+        lng: position[i + 1][1],
+      });
     }
+  };
+
+  // map関連
+  const googleMapsApiKey = process.env.REACT_APP_MAP_API_KEY;
+  useEffect(() => {
+    console.log("POSITIONS", positions);
+  });
+  const streetViewPanoramaOptions = {
+    position: { lat: positions.lat, lng: positions.lng },
+    pov: { heading: 100, pitch: 0 },
+    zoom: 1,
+    addressControl: true,
+    showRoadLabels: true,
+    zoomControl: true,
   };
   return (
     <div>
-      <Map1 />
+      <div
+        style={{
+          width: "800px",
+          height: "450px",
+          backgroundColor: "000000",
+        }}
+      >
+        <ReactStreetview
+          apiKey={googleMapsApiKey}
+          streetViewPanoramaOptions={streetViewPanoramaOptions}
+        />
+      </div>
       <div className="next_button_parent">
         {2 - i ? (
           <button onClick={incrementI}>次の場所へ</button>

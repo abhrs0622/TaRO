663808ff0a9cb2edf3988ed457140d81f7c26935
ayakakawabe <ayaka.kawabe@icloud.com>
2023-12-15 22:53:14 +0900

    change footer css

--- frontend/front/src/index.jsx
+++ frontend/front/src/index.jsx
@@ -18,7 +18,7 @@ root.render(
         <footer>
           <span style={{ margin: "15px 15px 15px 15px" }}>
             <a href="https://developer.yahoo.co.jp/sitemap/">
-              Webサービス by Yahoo! JAPAN
+              Web Services by Yahoo! JAPAN
             </a>
           </span>
         </footer>
